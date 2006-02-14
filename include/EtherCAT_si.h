bb321c3c5c4481daee566dd52a85237abdadc0b7
Florian Pose <fp@igh-essen.com>
2006-02-14 14:40:47 +0000

    EL5101-Inkrementalgeberklemme hinzugefügt.

--- include/EtherCAT_si.h
+++ include/EtherCAT_si.h
@@ -17,17 +17,17 @@
 
 /*****************************************************************************/
 
-#define EC_READ_EL31XX(SLAVE, CHANNEL) \
-    ((short int) ((EC_PROC_DATA(SLAVE)[(CHANNEL) * 3 + 2] << 8) | \
-                   EC_PROC_DATA(SLAVE)[(CHANNEL) * 3 + 1]))
-
-/*****************************************************************************/
-
 #define EC_WRITE_EL20XX(SLAVE, CHANNEL, VALUE) \
     do { \
         if (VALUE) EC_PROC_DATA(SLAVE)[0] |=  (1 << (CHANNEL)); \
         else       EC_PROC_DATA(SLAVE)[0] &= ~(1 << (CHANNEL)); \
     } while (0)
+
+/*****************************************************************************/
+
+#define EC_READ_EL31XX(SLAVE, CHANNEL) \
+    ((short int) ((EC_PROC_DATA(SLAVE)[(CHANNEL) * 3 + 2] << 8) | \
+                   EC_PROC_DATA(SLAVE)[(CHANNEL) * 3 + 1]))
 
 /*****************************************************************************/
 
@@ -39,14 +39,29 @@
 
 /*****************************************************************************/
 
-#define EC_READ_EL5001(SLAVE) \
+#define EC_CONF_EL5001_BAUD (0x4067)
+
+#define EC_READ_EL5001_STATE(SLAVE) \
+    ((unsigned char) EC_PROC_DATA(SLAVE)[0])
+
+#define EC_READ_EL5001_VALUE(SLAVE) \
     ((unsigned int) (EC_PROC_DATA(SLAVE)[1] | \
                      (EC_PROC_DATA(SLAVE)[2] << 8) | \
                      (EC_PROC_DATA(SLAVE)[3] << 16) | \
                      (EC_PROC_DATA(SLAVE)[4] << 24)))
 
-#define EC_READ_EL5001_STATE(SLAVE) \
+/*****************************************************************************/
+
+#define EC_READ_EL5101_STATE(SLAVE) \
     ((unsigned char) EC_PROC_DATA(SLAVE)[0])
+
+#define EC_READ_EL5101_VALUE(SLAVE) \
+    ((unsigned int) (EC_PROC_DATA(SLAVE)[1] | \
+                     (EC_PROC_DATA(SLAVE)[2] << 8)))
+
+#define EC_READ_EL5101_LATCH(SLAVE) \
+    ((unsigned int) (EC_PROC_DATA(SLAVE)[3] | \
+                     (EC_PROC_DATA(SLAVE)[4] << 8)))
 
 /*****************************************************************************/
 
