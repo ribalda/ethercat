9f055fa7ab9e0a24f40b604082472798d8616120
Florian Pose <fp@igh-essen.com>
2006-04-05 08:47:28 +0000

    Plugfest: Schönheitskorrekturen an Mailbox.

--- master/mailbox.h
+++ master/mailbox.h
@@ -16,9 +16,9 @@
 /*****************************************************************************/
 
 uint8_t *ec_slave_mbox_prepare_send(ec_slave_t *, uint8_t, size_t);
-int ec_slave_mbox_prepare_check(ec_slave_t *);
-int ec_slave_mbox_check(const ec_slave_t *);
-int ec_slave_mbox_prepare_fetch(ec_slave_t *);
+int      ec_slave_mbox_prepare_check(ec_slave_t *);
+int      ec_slave_mbox_check(const ec_slave_t *);
+int      ec_slave_mbox_prepare_fetch(ec_slave_t *);
 uint8_t *ec_slave_mbox_fetch(ec_slave_t *, uint8_t, size_t *);
 
 uint8_t *ec_slave_mbox_simple_io(ec_slave_t *, size_t *);
