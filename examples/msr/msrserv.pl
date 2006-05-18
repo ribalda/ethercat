d8c0244c5f0daf2e821b42720b02aee77dc75f02
Florian Pose <fp@igh-essen.com>
2006-05-18 12:35:47 +0000

    Changed license headers and added EtherCAT license notice.

--- examples/msr/msrserv.pl
+++ examples/msr/msrserv.pl
@@ -8,7 +8,8 @@
 #
 #  The IgH EtherCAT Master is free software; you can redistribute it
 #  and/or modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; version 2 of the License.
+#  as published by the Free Software Foundation; either version 2 of the
+#  License, or (at your option) any later version.
 #
 #  The IgH EtherCAT Master is distributed in the hope that it will be
 #  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -18,6 +19,15 @@
 #  You should have received a copy of the GNU General Public License
 #  along with the IgH EtherCAT Master; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+#  The right to use EtherCAT Technology is granted and comes free of
+#  charge under condition of compatibility of product made by
+#  Licensee. People intending to distribute/sell products based on the
+#  code, have to sign an agreement to guarantee that products using
+#  software based on IgH EtherCAT master stay compatible with the actual
+#  EtherCAT specification (which are released themselves as an open
+#  standard) as the (only) precondition to have the right to use EtherCAT
+#  Technology, IP and trade marks.
 #
 #------------------------------------------------------------------------------
 #
