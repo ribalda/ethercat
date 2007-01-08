3d3b4749b60d61bfb7429c9078cea688da5e347b
Florian Pose <fp@igh-essen.com>
2007-01-08 14:24:33 +0000

    Datagram preallocation with ATOMIC flag, because it is calles in timer context.

--- master/datagram.c
+++ master/datagram.c
@@ -115,7 +115,7 @@ int ec_datagram_prealloc(ec_datagram_t *datagram, /**< EtherCAT datagram */
         datagram->mem_size = 0;
     }
 
-    if (!(datagram->data = kmalloc(size, GFP_KERNEL))) {
+    if (!(datagram->data = kmalloc(size, GFP_ATOMIC))) {
         EC_ERR("Failed to allocate %i bytes of datagram memory!\n", size);
         return -1;
     }
