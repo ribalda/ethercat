62885ca1ac7e0dc504cbbc11228ad8c0f2caff8e
Florian Pose <fp@igh-essen.com>
2008-10-21 11:31:07 +0000

    Improved LGPL headers.

--- include/ecrt.h
+++ include/ecrt.h
@@ -1,33 +1,32 @@
 /******************************************************************************
  *
- *  $Id$
+ * $Id$
  *
- *  Copyright (C) 2006  Florian Pose, Ingenieurgemeinschaft IgH
+ * Copyright (C) 2006  Florian Pose, Ingenieurgemeinschaft IgH
  *
- *  This file is part of the IgH EtherCAT Master.
+ * This file is part of the IgH EtherCAT master userspace library.
+ * 
+ * The IgH EtherCAT master userspace library is free software; you can
+ * redistribute it and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation; version 2.1 of
+ * the License.
  *
- *  The IgH EtherCAT Master is free software; you can redistribute it
- *  and/or modify it under the terms of the GNU General Public License
- *  as published by the Free Software Foundation; either version 2 of the
- *  License, or (at your option) any later version.
+ * The IgH EtherCAT master userspace library is distributed in the hope that
+ * it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
  *
- *  The IgH EtherCAT Master is distributed in the hope that it will be
- *  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with the IgH EtherCAT Master; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- *
- *  The right to use EtherCAT Technology is granted and comes free of
- *  charge under condition of compatibility of product made by
- *  Licensee. People intending to distribute/sell products based on the
- *  code, have to sign an agreement to guarantee that products using
- *  software based on IgH EtherCAT master stay compatible with the actual
- *  EtherCAT specification (which are released themselves as an open
- *  standard) as the (only) precondition to have the right to use EtherCAT
- *  Technology, IP and trade marks.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the IgH EtherCAT master userspace library. If not, see
+ * <http://www.gnu.org/licenses/>.
+ * 
+ * The right to use EtherCAT Technology is granted and comes free of charge
+ * under condition of compatibility of product made by Licensee. People
+ * intending to distribute/sell products based on the code, have to sign an
+ * agreement to guarantee that products using software based on IgH EtherCAT
+ * master stay compatible with the actual EtherCAT specification (which are
+ * released themselves as an open standard) as the (only) precondition to have
+ * the right to use EtherCAT Technology, IP and trade marks.
  *
  *****************************************************************************/
 
--- lib/common.c
+++ lib/common.c
@@ -6,16 +6,16 @@
  * 
  * This file is part of the IgH EtherCAT master userspace library.
  * 
- * The IgH EtherCAT master userspace library is free software: you can
+ * The IgH EtherCAT master userspace library is free software; you can
  * redistribute it and/or modify it under the terms of the GNU Lesser General
- * Public License as published by the Free Software Foundation, version 2 of
+ * Public License as published by the Free Software Foundation; version 2.1 of
  * the License.
- * 
+ *
  * The IgH EtherCAT master userspace library is distributed in the hope that
  * it will be useful, but WITHOUT ANY WARRANTY; without even the implied
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with the IgH EtherCAT master userspace library. If not, see
  * <http://www.gnu.org/licenses/>.
