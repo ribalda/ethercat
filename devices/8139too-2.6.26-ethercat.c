d66c3833c5de908dfc5f06f05fcbad09fe9b88ff
Florian Pose <fp@igh-essen.com>
2009-03-09 08:42:49 +0000

    Replaced single uint16_t by u16.

--- devices/8139too-2.6.26-ethercat.c
+++ devices/8139too-2.6.26-ethercat.c
@@ -1425,7 +1425,7 @@ static void rtl_check_media (struct net_device *dev, unsigned int init_media)
 
 	if (tp->ecdev) {
 		void __iomem *ioaddr = tp->mmio_addr;
-		uint16_t state = RTL_R16(BasicModeStatus) & BMSR_LSTATUS;
+		u16 state = RTL_R16(BasicModeStatus) & BMSR_LSTATUS;
 		ecdev_set_link(tp->ecdev, state ? 1 : 0);
 	}
 	else {
