41f446038e171dac9a4a52946e5b5d741c6508e6
Martin Troxler <martin.troxler@komaxgroup.com>
2010-05-07 15:16:14 +0200

    Increase version magic due to changed ioctl interface: Slave port info, inplace I/O

--- master/ioctl.h
+++ master/ioctl.h
@@ -56,7 +56,7 @@
  *
  * Increment this when changing the ioctl interface!
  */
-#define EC_IOCTL_VERSION_MAGIC 5
+#define EC_IOCTL_VERSION_MAGIC 6
 
 // Command-line tool
 #define EC_IOCTL_MODULE                EC_IOR(0x00, ec_ioctl_module_t)
