d386419461c06fd8faf9d799dde72e7cdfc75727
Florian Pose <fp@igh-essen.com>
2011-05-12 08:56:05 +0200

    Removed "Pd: 0" output that does the initial pagefault. Write to the memory instead, to get rid of the output.

--- lib/master.c
+++ lib/master.c
@@ -455,7 +455,7 @@ int ecrt_master_activate(ec_master_t *master)
         }
 
         // Access the mapped region to cause the initial page fault
-        printf("pd: %x\n", master->process_data[0]);
+        master->process_data[0] = 0x00;
     }
 
     return 0;
