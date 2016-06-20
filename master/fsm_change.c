b565bfe81d6d80498b7ed1154c8bfc93afb2649d
Gavin Lambert <gavin.lambert@tomra.com>
2016-06-21 11:10:16 +1200

    Since ec_master_exec_slave_fsms checks the datagram state, these are redundant.

--- master/fsm_change.c
+++ master/fsm_change.c
@@ -126,15 +126,6 @@ int ec_fsm_change_exec(
 {
     if (fsm->state == ec_fsm_change_state_end || fsm->state == ec_fsm_change_state_error)
         return 0;
-    if (fsm->datagram &&
-            (fsm->datagram->state == EC_DATAGRAM_INIT ||
-             fsm->datagram->state == EC_DATAGRAM_QUEUED ||
-             fsm->datagram->state == EC_DATAGRAM_SENT)) {
-        // datagram not received yet
-        if (datagram != fsm->datagram)
-            datagram->state = EC_DATAGRAM_INVALID;
-        return 1;
-    }
 
     fsm->state(fsm, datagram);
 
