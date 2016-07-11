391f8b4c504909fee2cb862fc65564124eae3a41
Gavin Lambert <gavinl@compacsort.com>
2016-07-11 12:29:32 +1200

    Adds slave position to ecrt_slave_config_state.

--- master/slave_config.c
+++ master/slave_config.c
@@ -1284,11 +1284,13 @@ void ecrt_slave_config_state(const ec_slave_config_t *sc,
         state->al_state = sc->slave->current_state;
         state->error_flag = sc->slave->error_flag ? 1 : 0;
         state->ready = ec_fsm_slave_is_ready(&sc->slave->fsm) ? 1 : 0;
+        state->position = sc->slave->ring_position;
     } else {
         state->operational = 0;
         state->al_state = EC_SLAVE_STATE_UNKNOWN;
         state->error_flag = 0;
         state->ready = 0;
+        state->position = (uint16_t) -1;
     }
 }
 
