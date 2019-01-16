bd2c95dff0a94a2b0aa5d55d358d9fa06b317fa2
Daniel Gomez <daniel@qtec.com>
2019-01-16 11:41:31 +0100

    timer-setup: be able to setup timer in old kernels
    In older kernels, like in 4.4.38, timer_setup timer
    API is called setup_timer.
    
    Signed-off-by: Ricardo Ribalda Delgado <ricardo@ribalda.com>

--- examples/mini/mini.c
+++ examples/mini/mini.c
@@ -492,7 +492,11 @@ int __init init_mini_module(void)
 #endif
 
     printk(KERN_INFO PFX "Starting cyclic sample thread.\n");
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 14, 69)
+    setup_timer(&timer, cyclic_task, 0);
+#else
     timer_setup(&timer, cyclic_task, 0);
+#endif
     timer.expires = jiffies + 10;
     add_timer(&timer);
 
